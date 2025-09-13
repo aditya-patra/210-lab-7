05adc7970c8aead6a4f3fd0efa3067bb71bee915
Aditya Patra <adityabpatra@gmail.com>
2025-09-13 12:52:03 -0700

    Initial commit

--- main.cpp
+++ main.cpp
@@ -15,18 +15,21 @@ int main() {
     *(str+3) = "joshoey";
     *(str+4) = "joosh";
     reversed = reverseArray(str, SIZE);
-    displayArray(reversed, size);
+    cout << "Original Array: ";
+    displayArray(str, SIZE);
+    cout << "\nReversed Array: ";
+    displayArray(reversed, SIZE);
 }
 
 string* reverseArray(string* str, int SIZE) {
     string* newArr = new string[SIZE];
     for(int i = 0; i < SIZE; i++) {
-        *(newArr + i) = *(str - i - 1);
+        *(newArr + i) = *(str + SIZE - i - 1);
     }
     return newArr;
 }
 
-void displayArray(string *str, int SIZE) {
+void displayArray(string* str, int SIZE) {
     for(int i = 0; i < SIZE; i++) {
         cout << *(str + i) << " ";
     }
